cc6ae6d9fd896fdd1fd7204c1808b1fdbd013cc0
Unto Karila <unto.karila@gmail.com>
2021-08-19 10:03:41 +0300

    initial commit

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define pb push_back
+#define VI vector<int>
+#define VL vector<ll>
+#define MX vector<VI >
+#define all(x) x.begin(), x.end()
+#define IOS cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+
+int main(){
+
+
+
+	return 0;
+}
