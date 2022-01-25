25b4c00c4ff45a675e55d3d6eeee64fd3999ae35
Unto Karila <unto.karila@gmail.com>
2022-01-25 13:46:12 +0200

    fix matrix

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -3,7 +3,7 @@ template<typename T> struct Matrix{
     vector<vector<T> > mx;
 
     Matrix(int n, int m) : mx(vector<vector<T>>(n, vector<T>(m, 0))) {}
-    Matrix(vector<vector<T> > mx_) : mx(mx_) {}
+    Matrix(const vector<vector<T> > &mx_) : mx(mx_) {}
     Matrix(){}
 
     static Matrix<T> unit(int n){
@@ -12,63 +12,66 @@ template<typename T> struct Matrix{
         return x;
     }
     
-    int w() const { return mx.size(); }
-    int h() const { if(w()){ return mx[0].size(); } return 0; }
+    int r() const { return mx.size(); }
+    int c() const { return r() ? mx[0].size() : 0; }
 
     vector<T>& operator[](int i){ return mx[i]; }
     T operator()(int i, int j) const { return mx[i][j]; }
 
-    Matrix<T> &operator*=(const Matrix<T> &rhs){
-        if(h() == rhs.w()){
-            int n = w(), m = rhs.h(), l = h();
-            vector<vector<T>> nx(n, vector<T>(m, 0));
-            for(int i=0; i<n; i++){
-                for(int j=0; j<m; j++){
-                    for(int k=0; k<l; k++){
-                        nx[i][j] += mx[i][k]*rhs(k, j);
-                    }
+    Matrix<T> &operator*=(const Matrix<T> &lhs){
+        
+        assert(r() == lhs.c());
+        
+        int n = lhs.r(), m = c(), l = r();
+        vector<vector<T>> nx(n, vector<T>(m, 0));
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                for(int k=0; k<l; k++){
+                    nx[i][j] += lhs(i, k)*mx[k][j];
                 }
             }
-            mx.swap(nx);
-        } else {
-            mx.resize(0);
         }
+        
+        mx.swap(nx);
+        
         return *this;
     }
 
-    friend Matrix<T> operator*(Matrix<T> lhs, const Matrix<T> &rhs){
-        lhs *= rhs;
-        return lhs;
+    friend Matrix<T> operator*(const Matrix<T> &lhs, Matrix<T> rhs){
+        rhs *= lhs;
+        return rhs;
     }
 
-    T tolerance = 0;
-    Matrix<T> inv(){
+    T tolerance = 1e-10;
+    Matrix<T> inv(Matrix<T> rhs){
 
-        if(h() != w()) return unit(0);
+        assert(c() == r());
 
-        int n = w();
+        int n = r();
         Matrix<T> aux = *this, ret = unit(n);
-
+        
         auto reduce = [&](int i, int j, T d){
             for(int k=0; k<n; k++){
-                aux[i][k] -= d*aux[j][k];
-                ret[i][k] -= d*ret[j][k];
+                aux[j][k] -= d*aux[i][k];
+                ret[j][k] -= d*ret[i][k];
             }
         };
 
-        for(int j=0; j<n; j++){
-            for(int i=j; i<n; i++){
-                if(abs(aux[j][j]) < abs(aux[i][j])){
+        for(int i=0; i<n; i++){
+            
+            for(int j=i; j<n; j++){
+                if(abs(aux[i][i]) < abs(aux[j][i])){
                     aux[i].swap(aux[j]);
                     ret[i].swap(ret[j]);
                 }
             }
-            if(abs(aux[j][j]) <= tolerance) continue;
-            for(int i=0; i<n; i++) if(i != j) reduce(i, j, aux[i][j]/aux[j][j]);
-            reduce(j, j, (aux[j][j]-1)/aux[j][j]);
+
+            if(abs(aux[i][i]) <= tolerance) continue;
+            for(int j=0; j<n; j++) if(i != j) reduce(i, j, aux[j][i]/aux[i][i]);
+            reduce(i, i, (aux[i][i]-1)/aux[i][i]);
         }
         
         return ret;
     }
 };
-
